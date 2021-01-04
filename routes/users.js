09cf5f4dee752d65532fb4d3b7a1a4e4ca7ff940
Choi Dain <twa05157@gmail.com>
2021-01-05 00:10:14 +0900

    Chore: users.js 파일 삭제

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/userController');
-const jwtMiddlewares = require('../middlewares/middlewares');
-
-router.post('/signup', userController.signup);
-
-module.exports = router;
