5fe169119c826f8a324bc5bec8ebafd49a1c4485
Choi Dain <twa05157@gmail.com>
2021-01-05 01:02:50 +0900

    Feat: 로그인

--- modules/responseMessage.js
+++ modules/responseMessage.js
@@ -1,26 +1,26 @@
 module.exports = {
   NULL_VALUE: "필요한 값이 없습니다.",
   OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
-  INTERNAL_SERVER_ERROR: "서버 내부 에러 입니다",
+  INTERNAL_SERVER_ERROR: "서버 내부 에러 입니다.",
 
   // 회원가입
-  SIGNUP_SUCCESS: "회원가입 성공입니다",
+  SIGNUP_SUCCESS: "회원가입 성공입니다.",
 
   // 로그인
-  NOT_FOUND_USER: "존재하지 않는 유저입니다",
-  SINGIN_SUCCESS: "로그인 성공입니다",
+  NOT_FOUND_USER: "존재하지 않는 유저입니다.",
+  SINGIN_SUCCESS: "로그인 성공입니다.",
 
   // 닉네임 변경
-  NICKNAME_UPDATE_SUCCESS: "닉네임 변경 성공입니다",
+  NICKNAME_UPDATE_SUCCESS: "닉네임 변경 성공입니다.",
 
   // 유저 첫 번째 탭 조회
-  USER_HOME_SUCCESS: "유저 홈 조회 성공입니다",
+  USER_HOME_SUCCESS: "유저 홈 조회 성공입니다.",
 
   // TOKEN MESSAGE
-  EMPTY_TOKEN : "토큰 값이 없습니다",
-  EXPIRED_TOKEN : "토큰 값이 만료되었습니다",
-  INVALID_TOKEN : "유효하지 않은 토큰값입니다",
-  AUTH_SUCCESS : "인증에 성공했습니다",
-  ISSUE_SUCCESS: "새로운 토큰이 생성되었습니다",
-  SUCCESS_TOKEN_REPLACEMENT: "토큰 재발급 했습니다",
+  EMPTY_TOKEN : "토큰 값이 없습니다.",
+  EXPIRED_TOKEN : "토큰 값이 만료되었습니다.",
+  INVALID_TOKEN : "유효하지 않은 토큰값입니다.",
+  AUTH_SUCCESS : "인증에 성공했습니다.",
+  ISSUE_SUCCESS: "새로운 토큰이 생성되었습니다.",
+  SUCCESS_TOKEN_REPLACEMENT: "토큰 재발급 했습니다.",
 }
--- routes/users/index.js
+++ routes/users/index.js
@@ -4,5 +4,6 @@ const userController = require('../../controller/userController');
 const jwtMiddlewares = require('../../middlewares/middlewares');
 
 router.post('/signup', userController.signup);
+router.post('/signin', userController.signin);
 
 module.exports = router;
