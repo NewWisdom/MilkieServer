b094685ae58d57837853760a1c0627ab2a32ebca
NewWisdom <wlgp2500@gmail.com>
2021-01-05 17:01:02 +0900

    Style: manage를 report로 변경

--- routes/report/index.js
+++ routes/report/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const jwtMiddleware = require('../../middlewares/middlewares');
-const manageController = require('../../controller/reportController');
+const reportController = require('../../controller/reportController');
 
-router.post('/:cafeId/deleteCafe', jwtMiddleware.userJwt, manageController.deleteCafe);
-router.post('/:cafeId/editCafe', jwtMiddleware.userJwt, manageController.editCafe);
-router.get('/', jwtMiddleware.userJwt, manageController.readReports);
+router.post('/:cafeId/deleteCafe', jwtMiddleware.userJwt, reportController.deleteCafe);
+router.post('/:cafeId/editCafe', jwtMiddleware.userJwt, reportController.editCafe);
+router.get('/', jwtMiddleware.userJwt, reportController.readReports);
 
 module.exports = router;
