d601bdeab3e2153e714804cd462941e71914196d
NewWisdom <wlgp2500@gmail.com>
2021-01-07 14:28:38 +0900

    Refactor: cafeInfo, menu 데이터 포맷 가공

--- controller/cafeController.js
+++ controller/cafeController.js
@@ -2,6 +2,7 @@ const util = require('../modules/util');
 const responseMessage = require('../modules/responseMessage');
 const statusCode = require('../modules/statusCode');
 const { cafeService, universeService } = require('../service');
+const honeyTip = require('../models/honeyTip');
 // const cafe = require('../models/cafe');
 
 module.exports = {
@@ -14,15 +15,19 @@ module.exports = {
     }
 
     try {
-      const cafeInfo = await cafeService.readCafeInfo(cafeId);
+      var cafeInfo = await cafeService.readCafeInfo(cafeId);
       if (!cafeInfo) {
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NOT_EXISTING_CAFE_INFO));
       }
+      cafeInfo[0]['honeyTip'] = cafeInfo[0]['hasCafe.id'];
+      delete cafeInfo[0]['hasCafe.id'];
 
       const menu = await cafeService.readCafeMenu(cafeId);
       if (!menu) {
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NOT_EXISTING_CAFE_MENU));
       }
+      menu[0]['category'] = menu[0]['hasMenu.categoryId'];
+      delete menu[0]['hasMenu.categoryId'];
 
       const universe = await universeService.readUniverseCount(cafeId);
       if (!universe) {
