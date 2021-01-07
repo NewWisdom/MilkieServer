45630e0299680a1cbb9e4f57f0828ab38195b28e
NewWisdom <wlgp2500@gmail.com>
2021-01-08 03:31:32 +0900

    Refactor: res 데이터 형식 가공

--- controller/cafeController.js
+++ controller/cafeController.js
@@ -2,8 +2,6 @@ const util = require('../modules/util');
 const responseMessage = require('../modules/responseMessage');
 const statusCode = require('../modules/statusCode');
 const { cafeService, universeService } = require('../service');
-const honeyTip = require('../models/honeyTip');
-// const cafe = require('../models/cafe');
 
 module.exports = {
   readOneCafeInfo: async (req, res) => {
@@ -15,19 +13,27 @@ module.exports = {
     }
 
     try {
-      var cafeInfo = await cafeService.readCafeInfo(cafeId);
+      let cafeInfo = await cafeService.readCafeInfo(cafeId);
       if (!cafeInfo) {
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NOT_EXISTING_CAFE_INFO));
       }
-      cafeInfo[0]['honeyTip'] = cafeInfo[0]['hasCafe.id'];
-      delete cafeInfo[0]['hasCafe.id'];
-
-      const menu = await cafeService.readCafeMenu(cafeId);
+      /** 데이커 가공 */
+      const honeyTips = []
+      for (let i = 0; i < cafeInfo.length; i++) {
+        honeyTips.push(cafeInfo[i]['hasCafe.id'])
+        delete cafeInfo[i]['hasCafe.id'];
+      }
+      cafeInfo = cafeInfo[0]
+      cafeInfo['honeyTip'] = honeyTips;
+    
+      let menu = await cafeService.readCafeMenu(cafeId);
       if (!menu) {
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NOT_EXISTING_CAFE_MENU));
       }
-      menu[0]['category'] = menu[0]['hasMenu.categoryId'];
-      delete menu[0]['hasMenu.categoryId'];
+      for (let i = 0; i < menu.length; i++) {
+        menu[i]['category'] = menu[i]['hasMenu.categoryId'];
+        delete menu[i]['hasMenu.categoryId'];
+      }
 
       const universe = await universeService.readUniverseCount(cafeId);
       if (!universe) {
