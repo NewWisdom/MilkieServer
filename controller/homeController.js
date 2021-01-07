820ecbaff6c4e3226feb5e2a8e5101b65ea9a2f5
Choi Dain <twa05157@gmail.com>
2021-01-08 03:33:07 +0900

    Chore: categoryCafe 빈 배열로 줘서 fail 코드 삭제

--- controller/homeController.js
+++ controller/homeController.js
@@ -87,10 +87,6 @@ module.exports = {
         }
       });
 
-      if (!categoryCafe) {
-        res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NOT_FOUND_CATEGORY_CAFE));
-      }
-
       const userNickName = await user.findAll({
         attributes: ['nickName'],
         where: {
