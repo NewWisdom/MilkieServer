2dd77c6bf13dab1e490cbf96f10e520e8578eeb8
Choi Dain <twa05157@gmail.com>
2021-01-12 03:08:50 +0900

    수정중

--- routes/home/index.js
+++ routes/home/index.js
@@ -4,6 +4,6 @@ const homeController = require('../../controller/homeController');
 const jwtMiddlewares = require('../../middlewares/middlewares');
 
 router.get('/milkyHome', jwtMiddlewares.userJwt, homeController.milkyHome);
-router.get('/categoryHome/:categoryId', jwtMiddlewares.userJwt, homeController.categoryHome);
+router.get('/categoryHome', jwtMiddlewares.userJwt, homeController.categoryHome);
 
 module.exports = router;
