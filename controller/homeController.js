96e238edbbd78d851def4e2d71c0b06ab3cf5d5e
Choi Dain <twa05157@gmail.com>
2021-01-13 01:11:54 +0900

    Fix: 카테고리에 해당하는 카페 없을 때 오류 메시지 반환

--- controller/homeController.js
+++ controller/homeController.js
@@ -59,8 +59,8 @@ module.exports = {
         },
       });
 
-      if (!findCategoryResult) {
-        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+      if (findCategoryResult.length == 0) {
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NOT_SEARCH_CAFE));
       }
 
       var findCategoryArray = [];
