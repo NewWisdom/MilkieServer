fb3c6ce2356ccd91b99aeb4b37ff5895139826b9
Choi Dain <twa05157@gmail.com>
2021-01-12 23:30:59 +0900

    카테고리 아직 수정중

--- controller/homeController.js
+++ controller/homeController.js
@@ -3,7 +3,6 @@ const responseMessage = require("../modules/responseMessage");
 const util = require("../modules/util");
 const { user, cafe, universe, menuCategory, menu, sequelize } = require('../models/index');
 const jwt = require('../modules/jwt');
-const { universeService } = require('../service');
 
 module.exports = {
   milkyHome: async (req, res) => {
@@ -94,14 +93,56 @@ module.exports = {
       }
 
       const findMenu = findMenuArray;
+      console.log(findMenuArray);
 
-      const categoryCafe = await cafe.findAll({
-        attributes: ['id', 'cafeName', 'cafeAddress', 'businessHours', 'longitude', 'latitude', 'isReal'],
-        where: {
-          id : findMenu,
-          isReal: true
+      const tempResult = await sequelize.query(`SELECT CAFE.id, CAFE.cafeName, ifnull(universeCount, 0) universeCount, CAFE.longitude, CAFE.latitude, CAFE.cafeAddress, CAFE.businessHours,
+      ifnull(isUniversed, false) as isUniversed
+              FROM CAFE LEFT JOIN (
+                  SELECT distinct UNIVERSE.cafeId, count(UNIVERSE.cafeId) universeCount, true as isUniversed
+                  FROM UNIVERSE
+                  WHERE userId = ${userIdx}
+              ) as cu ON CAFE.id = cu.cafeId
+              where CAFE.id in ${findMenu} and CAFE.isReal = true;`);
+
+      console.log(tempResult);
+      const result = tempResult[0];
+      console.log(result);
+
+      for (let i = 0; i < result.length; i++) {
+        if (result[i].isUniversed == 1) {
+          result[i].isUniversed = true
+        } else {
+          result[i].isUniversed = false
         }
-      });
+      }
+
+      // const temp = await sequelize.query(`SELECT MENU.menuId
+      //         FROM MENU LEFT JOIN (
+      //             SELECT distinct MENU_CATEGORY.menuId
+      //             FROM MENU_CATEGORY
+      //             WHERE categoryId = ${categoryId}
+      //         ) as mu ON MENU.menuId = mu.menuId`);
+
+      // const category = temp[0];
+      // for (let i = 0; i < category.length; i++) {
+      //   temp.push(temp[i].menuId)
+      // }
+      // console.log(category);
+      // FROM CAFE LEFT JOIN (
+      //   SELECT CAFE.id
+      //   FROM MENU
+      //   WHERE userId = ${userIdx}
+      // ) as mu On CAFE.id = mu.cafeId
+      // where CAFE.id = ${[findMenu]};
+
+      // console.log(tempResult);
+      // for (let i = 0; i < categoryCafe.length; i++) {
+      //   if (categoryCafe[i].id == 0) {
+      //     categoryCafe = []
+      //   } else {
+      //     categoryCafe
+      //   }
+      // }
 
       const userNickName = await user.findAll({
         attributes: ['nickName'],
@@ -113,7 +154,7 @@ module.exports = {
       const { nickName } = userNickName[0];
 
       res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.USER_HOME_SUCCESS, {
-        categoryCafe,
+        result,
         nickName
       }));
       return;
