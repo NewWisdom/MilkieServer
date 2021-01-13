d11498274a6bf1baf041195b7d67f108aa1c3739
Choi Dain <twa05157@gmail.com>
2021-01-13 15:47:33 +0900

    Fix: universeOff cafeId params로 받기

--- routes/universe/index.js
+++ routes/universe/index.js
@@ -4,7 +4,7 @@ const universeController = require('../../controller/universeController');
 const jwtMiddlewares = require('../../middlewares/middlewares');
 
 router.post('/universeOn', jwtMiddlewares.userJwt, universeController.universeOn);
-router.delete('/universeOff', jwtMiddlewares.userJwt, universeController.universeOff);
+router.delete('/:cafeId', jwtMiddlewares.userJwt, universeController.universeOff);
 router.get('/universeHome', jwtMiddlewares.userJwt, universeController.universeHome);
 
 module.exports = router;
